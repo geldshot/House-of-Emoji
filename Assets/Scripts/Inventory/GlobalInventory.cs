ed831bdc12e4435c92fc3255c249ac6472c872c4
Geldshot <geldshot@gmail.com>
2014-12-07 22:25:52 -0800

    Committing dialogue changes and a new inventory object + script.

---
+++ Assets/Scripts/Inventory/GlobalInventory.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GlobalInventory : MonoBehaviour {
+	private Dictionary<string,GameObject> _items;
+	// Use this for initialization
+	void Start () {
+		_items = new Dictionary<string, GameObject>();
+		foreach(Transform child in transform){
+			_items.Add(child.name,child.gameObject);
+			child.gameObject.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void activateItem(string name){
+		GameObject obj = _items[name];
+		if(obj != null){
+			obj.SetActive(true);
+		}else{
+			Debug.Log (name + " is not a valid item");
+		}
+	}
+	
+	public bool haveItem(string name){
+		GameObject obj = _items[name];
+		if(obj != null){
+			return obj.activeSelf;
+		}
+		Debug.Log (name + " is not a valid item");
+		return false;
+	}	
+}
